99ba72c8d0b6374035a0259ceb8e39dbb9262393
Jamalarm <jamalarm@googlemail.com>
2015-06-24 23:41:41 +0100

    Updated Readme and added usage instructions

--- src/main/java/net/thoughtmachine/Application.java
+++ src/main/java/net/thoughtmachine/Application.java
@@ -9,10 +9,12 @@ import java.io.*;
 public class Application {
 
     public static void main(String[] args) throws IOException {
+        //Add your input file here
         final GameController controller = GameControllerFactory.getFileBasedTestInstance("src/test/resources/testStateInputFiles/testState1.txt");
 
         controller.runGame();
 
+        //Set your output file here
         controller.writeGameState(new GameStateFileWriter(new FileWriter("target/testout.txt")));
     }
 }
